4bd8e45d3d4281b405f3b733ff9a2f6e325d8678
6272 <6272user@6272s-Mac-mini-7.local>
2016-09-29 10:57:22 +0700

    Initial commit

---
+++ app/src/main/java/com/egci392/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.egci392.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements TopFragment.TopSectionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    public void createMessage(String top,String bottom){
+        BottomFragment bottomFragment = (BottomFragment)getSupportFragmentManager().findFragmentById(R.id.fragment2);
+        bottomFragment.setMessage(top,bottom);
+    }
+}
